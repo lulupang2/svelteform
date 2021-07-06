7310c9767d81449b021867f5a3f11220c772f61b
lulupang2 <lulupang@live.co.kr>
2021-07-06 22:40:20 +0900

    6

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,100 @@
 <script>
-	export let name;
-	let isRed = false
+    export let name;
+    let isRed = false
 </script>
 
 
 <main>
-	<h1>회원가입</h1>
-	<div class =box>
-		<form action="login.jsp" method="post" >
-		<input type="text" name="" id="" placeholder="아이디를 입력하세요">
-		<input type="password" name="" id="" placeholder="비밀번호를 입력하세요">
-		</form>
+    <h1>회원가입</h1>
+    <div class=box>
 
-	</div>
+        <form action="login.jsp" method="post">
+            <input type="text" name="" id="" placeholder="아이디를 입력하세요">
+            <input type="password" name="" id="" placeholder="비밀번호를 입력하세요">
+            <br><input type="button" value="로그인">
+            <input type="button" value="회원가입">
+        </form>
+
+    </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        display: grid;
+        place-items: center;
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
+        min-width: 100vw;
+    }
 
-	.box{
-		margin : 0 auto;
-		background-color: #ff3333;
-		width: 640px;
-		height: 480px;
-		border-radius: 40px;
+    .box {
+        display: grid;
+        place-items: center;
+        background-color: #ff6655;
+        width: 640px;
+        height: 480px;
+        border-radius: 40px;
 
-	}
-	.box input { display: flex;}
-	.box input[type=text] {
-		border:0px;
-	}
-	.box input[type=password] {
-		border:0px;
-	}
+    }
+
+    .box input::placeholder {
+        color: #fff;
+
+    }
+    input:focus{
+        outline: none;
+        caret-color: #ff6655;
+    }
+
+    .box input[type=text] {
+        border: 1px solid #fff;
+        background-color: #ff6655;
+        padding-left: 10px;
+        border-radius: 70px;
+        width: 50vw;
+        height: 50px;
+        color: #fff;
+    }
+
+    .box input[type=password] {
+        border: 1px solid #fff;
+        background-color: #ff6655;
+        padding-left: 10px;
+        border-radius: 70px;
+        width: 50vw;
+        height: 50px;
+        top: 50%;
+        color: #fff;
+
+    }
+
+    .box input[type=button] {
+        margin-top: 1vh;
+        border-radius: 10px;
+        color: #ff6655;
+        font-weight: bolder;
+        width: 25vw;
+        height: 35px;
+        border: 0;
+        background-color: #fff;
+
+    }
+    .box input[type=button]:hover{
+        cursor: pointer;
+    }
 
 
-	h1 {
-		color: #686;
-		text-transform: uppercase;
-		font-size: 2em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #686;
+        text-transform: uppercase;
+        font-size: 2em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
